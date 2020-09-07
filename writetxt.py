dfe66a901a7779d6071a2cf7c9a97e12b315d67a
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2020-09-07 23:11:29 +0200

    Delete writetxt.py

--- writetxt.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 31 21:36:24 2017
-
-@author: Carlo
-"""
-
-with open("c:\users\carlo\desktop\python\cane.txt","r+") as my_file:
-    my_file.write("welcome to hell")

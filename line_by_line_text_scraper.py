1415ae5142da09f54573b99c4345759bde9c0b2e
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-08-24 15:05:21 +0200

    Update and rename line_by_line_text_scraper to line_by_line_text_scraper.py

--- line_by_line_text_scraper.py
+++ line_by_line_text_scraper.py
@@ -7,3 +7,4 @@ if i <= (len(lines)-3):
     while i in range(len(lines)):
         print (f"Hello, your name is {lines[i][:-1]}, you are {lines[i+1][:-1]} years old and you likes: {lines[i+2][:-1]}")
         i +=4
+file1.close()

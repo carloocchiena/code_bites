3daa1fa488ac14d134a6e41fa21bba7c71a84cfd
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-09-28 12:32:33 +0200

    Update and rename c#_functional.cs to c#_lambda_funcl.cs

--- c#_lambda_funcl.cs
+++ c#_lambda_funcl.cs
@@ -23,3 +23,30 @@ namespace AlternateExpressions
     }
   }
 }
+
+////////////////////////
+
+using System;
+
+namespace AlternateExpressions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] spaceRocks = {"meteoroid", "meteor", "meteorite"};
+      
+      bool makesContact = Array.Exists(spaceRocks, (string s) => s == "meteorite");
+      
+      if (makesContact)
+      {
+        Console.WriteLine("At least one space rock has reached the Earth's surface!");
+      } 
+    } 
+    
+    static bool HitGround(string s)
+    {
+      return s == "meteorite";
+    }
+  }
+}

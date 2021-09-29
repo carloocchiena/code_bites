f1e479de227ba981df577eb60e7afbab27f33758
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-09-29 15:15:58 +0200

    Create c#_do_while.cs

---
+++ c#_do_while.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DoWhileLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool buttonClick = true;
+      
+      do {
+        Console.WriteLine("DRIIN");
+      } while (!buttonClick);
+
+        Console.WriteLine("Time for a five minute break!");
+      }
+
+    }
+  }

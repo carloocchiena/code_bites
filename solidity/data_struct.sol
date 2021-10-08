5d4ccafeeadf92107292ebcfaf020744f79cff7f
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-10-08 12:13:41 +0200

    Update data_struct.sol

--- solidity/data_struct.sol
+++ solidity/data_struct.sol
@@ -8,8 +8,10 @@ contract smart_contract {
 
     // define non-negative 256 bits int value
     // define a data structure that would allow key-value data type with mapping
+    // define a time variable
     uint256 public people_count = 0;
     mapping(uint => Person) public people;
+    uint256 start_time;
     
     // define the variable to store the owner of the smart_contract
     address owner;
@@ -20,6 +22,18 @@ contract smart_contract {
         _;
     }
     
+    // set the star time variable value 
+    constructor() public {
+    start_time = 1633687408; //epoch for 08 october, 10.03
+    }
+    
+    
+    // check if the contract is open 
+    modifier only_while_open() {
+        require(block.timestamp >= start_time);
+        _;
+    }
+    
     // define our data structure
     struct Person {
         uint _id;
@@ -27,7 +41,7 @@ contract smart_contract {
         string _last_name;
     }
     
-    
+
     //function needed to increase people_count, just for internal access only
     function increment_count() internal {
         people_count++;
@@ -37,6 +51,7 @@ contract smart_contract {
     function add_person(string memory _first_name, string memory _last_name) 
         public 
         only_owner
+        only_while_open
     {
         increment_count(); 
         people[people_count] = Person(people_count, _first_name, _last_name);

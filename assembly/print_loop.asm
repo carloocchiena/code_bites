9208781b7ab2d63b5aadad7500380ded88b96e64
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-10-14 17:13:06 +0200

    Create print_loop.asm

---
+++ assembly/print_loop.asm
@@ -0,0 +1,29 @@
+section .bss
+    num resb 1
+
+section .text
+    global _start
+    
+_start:
+    mov ecx, 10
+    mov eax, "1"
+    
+loop_one:
+    mov [num], eax
+    mov eax, 4 
+    mov ebx, 1
+    push ecx
+    
+    mov ecx, num
+    mov edx, 1
+    int 0x80
+    
+    mov eax, [num]
+    sub eax, "0"
+    inc eax
+    add eax, "0"
+    pop ecx
+    loop loop_one
+    
+    mov eax,1 
+    int 0x80

45172a58aa2068ab85f07794551c09c5c2dd96d5
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-11-18 13:14:01 +0100

    Create functions.ts

---
+++ typescript/functions.ts
@@ -0,0 +1,17 @@
+// Named function
+function add(x, y) {
+  return x + y;
+}
+ 
+// Anonymous function
+let myAdd = function (x, y) {
+  return x + y;
+};
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+ 
+let myAdd = function (x: number, y: number): number {
+  return x + y;
+};

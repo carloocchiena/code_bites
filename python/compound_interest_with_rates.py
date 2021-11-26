d7287d88a67a9c1e1a4ba085172560b282182582
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-11-26 15:36:58 +0100

    Create compound_interest_with_rates.py

---
+++ python/compound_interest_with_rates.py
@@ -0,0 +1,17 @@
+initial = 50000
+recurring = 500
+interest = 0.1
+maturity = 4
+
+capital_amount = initial * (1 + interest)**maturity
+
+recurring_amount = 0
+
+maturity -= 1 # this to evaluate the recurring amout at the END of the first year
+
+while maturity > 0:
+    temp = recurring * (1 + interest)**maturity
+    maturity -= 1
+    recurring_amount += temp
+    
+print(capital_amount + recurring_amount + recurring)    

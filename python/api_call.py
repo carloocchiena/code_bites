d34c1ed4151f5d570cf69aa7f1d56597987ac8e2
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-12-23 23:21:31 +0100

    Delete api_call.py

--- python/api_call.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-user = "test@yopmail.com"
-token = "eyJraWQiOiJFbFQ5UG9WM25jN2VpVlwv"
-headers = {"Authorization": "Bearer" + f" {token}"}
-
-url = "https://api.example.com/v1/products"
-requests.get(url, headers=headers)

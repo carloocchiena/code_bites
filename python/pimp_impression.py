bb11a5f27f06aea69dbd4eb82f51a35944789b97
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-02-22 19:43:40 +0100

    Create pimp_impression.py

---
+++ python/pimp_impression.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service     
+from webdriver_manager.chrome import ChromeDriverManager  
+
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+for i in range(1000):
+    time.sleep(1)
+    driver.get(url)
+    

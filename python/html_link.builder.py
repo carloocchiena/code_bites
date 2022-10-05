bc0d650d4d917f570be2a6cea99ba3e673d74609
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-10-05 22:22:55 +0200

    Update html_link.builder.py

--- python/html_link.builder.py
+++ python/html_link.builder.py
@@ -1,7 +1,7 @@
 #hyperlink HTML builder
 
-linkurl=input("Enter your URL:\n")
-linkname=input ("Enter the link name:\n")
+linkurl = input("Enter your URL:\n")
+linkname = input ("Enter the link name:\n")
 
-print ("Here's your HTML string:\n")
-print (f'<a href="{linkurl}">{linkname}</a>')
+print("Here's your HTML string:\n")
+print(f'<a href="{linkurl}">{linkname}</a>')

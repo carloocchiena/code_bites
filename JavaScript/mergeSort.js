cc5b1ae47ef3f0f2eea7f2ad9458c1cf4e8d9d25
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-10-19 12:32:45 +0200

    Create mergeSort.js

---
+++ JavaScript/mergeSort.js
@@ -0,0 +1,26 @@
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  
+  const middleIndex = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, middleIndex);
+  const rightArr = arr.slice(middleIndex, arr.length);
+  
+  return merge(mergeSort(leftArr), mergeSort(rightArr))
+  
+  function merge(leftArr, rightArr) {
+    let resultArr = []
+    let leftIndex = 0
+    let rightIndex = 0
+    
+while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+  if (leftArr[leftIndex] < rightArr[rightIndex]) {
+   resultArr.push(leftArr[leftIndex]);
+   leftIndex ++ 
+  } else {
+  resultArr.push(rightArr[rightIndex]);
+  }
+ }
+}
+  

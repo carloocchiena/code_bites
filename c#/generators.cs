01bc0eb31627b26971371aff9a31edc941775e3e
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-10-19 21:54:42 +0200

    Create generators.cs

---
+++ c#/generators.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace csharp_generators
+{
+    public class Generators
+    {
+        private IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+
+            foreach (var number in _numbers)
+                yield return number;
+        }
+
+        private IEnumerable<int> _numbers = new[] { 6, 7, 8, 9, 10 };
+
+        [Fact]
+        public void Simple_Yield()
+        {
+            foreach (var result in GetNumbers())
+            {
+                Trace.WriteLine(result);
+            }
+        }
+
+        public class WebRequester
+        {
+            public IEnumerable<double> GetValues()
+            {
+                yield return -1;
+
+                while (true)
+                {
+                    yield return SensorData.GetDataSync("iddqd");
+                    yield return SensorData.GetDataSync("idkfa");
+                }
+            }
+        }
+
+        [Fact]
+        public void Generator_Sync()
+        {
+            var hello = new WebRequester();
+
+            foreach (var result in hello.GetValues())
+            {
+                Trace.WriteLine(result);
+
+                if (result > 0)
+                    break;
+            }
+        }
+
+        public class WebRequesterAsync
+        {
+            public IEnumerable<Task<double>> GetValues()
+            {
+                while (true)
+                {
+                    yield return Task.FromResult(-1d);
+                    yield return SensorData.GetDataAsync("iddqd");
+                    yield return SensorData.GetDataAsync("idkfa");
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Generator_Async()
+        {
+            var hello = new WebRequesterAsync();
+
+            foreach (var result in hello.GetValues())
+            {
+                Trace.WriteLine(await result);
+            }
+        }
+    }
+}
+
+About
+csharp-tutorial/14_Generators.cs at master · ttu/csharp-tutorial

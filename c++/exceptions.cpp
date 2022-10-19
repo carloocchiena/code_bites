1f8e17214941d4dbbc023b649ca27e2cc37fd1ba
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-10-19 13:01:22 +0200

    Create exceptions.cpp

---
+++ c++/exceptions.cpp
@@ -0,0 +1,36 @@
+ #include<iostream>
+ using namespace std;
+
+ class DivisionByZeroException
+ {
+    public:
+        void getErrorMessage()
+        {
+            cout<<"Error : Do not divide by zero !"<<endl;
+        }
+ };
+
+ double division(double a,double b) throw(DivisionByZeroException)
+ {
+     if(b==0)
+        throw DivisionByZeroException();
+    return a/b;
+ }
+
+ int main() {
+    double result;
+    try {
+        result = division(13,0);
+        cout<<"The result is = "<<result<<endl;
+    }
+    //This catch block handles DivisionByZeroException type of exception only
+    catch(DivisionByZeroException e) {
+        e.getErrorMessage();
+    }
+    //This catch block handles all other types of exceptions
+    catch(...)
+    {
+        cout<<"Error !"<<endl;
+    }
+    return 0;
+}

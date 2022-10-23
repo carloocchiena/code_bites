51712b7f54bb08ced54c5093151fd70b1436a3f5
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2022-10-23 18:21:53 +0200

    Create web3.py

---
+++ python/web3.py
@@ -0,0 +1,60 @@
+from web3 import Web3
+
+
+w3 = Web3(Web3.HTTPProvider("https://cloudflare-eth.com"))
+
+dai_token_addr = "0x6B175474E89094C44Da98b954EedeAC495271d0F"     # DAI
+weth_token_addr = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"    # Wrapped ether (WETH)
+
+acc_address = "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11"        # Uniswap V2: DAI 2
+
+# questa è un'ABI (Contract Application Binary Interface) semplificata per un contratto token ERC-20.
+# Espone solo i metodi balanceOf(address), decimals(), symbol() e totalSupply()
+simplified_abi = [
+    {
+        'inputs': [{'internalType': 'address', 'name': 'account', 'type': 'address'}],
+        'name': 'balanceOf',
+        'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [],
+        'name': 'decimals',
+        'outputs': [{'internalType': 'uint8', 'name': '', 'type': 'uint8'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [],
+        'name': 'symbol',
+        'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [],
+        'name': 'totalSupply',
+        'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    }
+]
+
+dai_contract = w3.eth.contract(address=w3.toChecksumAddress(dai_token_addr), abi=simplified_abi)
+symbol = dai_contract.functions.symbol().call()
+decimals = dai_contract.functions.decimals().call()
+totalSupply = dai_contract.functions.totalSupply().call() / 10**decimals
+addr_balance = dai_contract.functions.balanceOf(acc_address).call() / 10**decimals
+
+#  DAI
+print("===== %s =====" % symbol)
+print("Total Supply:", totalSupply)
+print("Addr Balance:", addr_balance)
+
+weth_contract = w3.eth.contract(address=w3.toChecksumAddress(weth_token_addr), abi=simplified_abi)
+symbol = weth_contract.functions.symbol().call()
+decimals = weth_contract.functions.decimals().call()
+totalSupply = weth_contract.functions.totalSupply().call() / 10**decimals
+addr_balance = weth_contract.functions.balanceOf(acc_address).call() / 10**decimals
+
+#  WETH
+print("===== %s =====" % symbol)
+print("Total Supply:", totalSupply)
+print("Addr Balance:", addr_balance)

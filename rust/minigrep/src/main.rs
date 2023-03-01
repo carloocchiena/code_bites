5aa02e1ba801c5ff047ff1b52246225aad3bfd77
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2023-03-01 19:36:01 +0100

    Add files via upload

---
+++ rust/minigrep/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+
+  let config = Config::build(&args).unwrap_or_else(|err| {
+    eprintln!("Problem parsing arguments: {err}");
+    process::exit(1);
+  });
+
+  println!("Searching for: '{}'\n", config.query);
+  println!("In file: '{}'\n", config.file_path);
+  println!("The result is:\n");
+
+  if let Err(e) = minigrep::run(config) {
+    eprintln!("Application error: {e}");
+    process::exit(1);
+  }
+  
+}

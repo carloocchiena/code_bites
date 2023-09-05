f297c69d3413b5299f05e9384c2da71ec10efe8e
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2023-09-05 15:30:48 +0200

    Create soapAPI.php

---
+++ php/soapAPI.php
@@ -0,0 +1,28 @@
+<?php
+
+$utente = 'USER';
+$password = 'PSW';
+
+$wsdl_url = 'https://example.com?wsdl';
+
+$client = new SoapClient($wsdl_url, array('trace' => 1));
+
+$params = array(
+    'userName' => $utente,
+    'password' => $password
+);
+
+try {
+    
+    $token_response = $client->getToken(array('USERNAME' => $utente, 'PASSWORD' => $password));
+
+    $token = $token_response->getTokenResult;
+    
+    echo $token;
+} catch (SoapFault $e) {
+ 
+    echo "Errore: " . $e->getMessage();
+}
+
+echo $token
+?>
